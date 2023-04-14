d53ae2706aeeaad15dea3a3e3a1a5af5a884066c
MarsIfeanyi <marschalice@gmail.com>
2023-04-14 23:30:46 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Gallery from "./component/gallery";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-700 text-2xl text-center  mt-20 "></h1>
+
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
