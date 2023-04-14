b7f69991c2ad4dfa99aa95f630404a26e6a94917
MarsIfeanyi <marschalice@gmail.com>
2023-04-14 23:33:06 +0100

    Initial Commit

---
+++ src/assets/index.js
@@ -0,0 +1,48 @@
+import AvengerSpider from "./AvengeSpider.png";
+import book1 from "./book1.png";
+import book2 from "./book2.png";
+import book3 from "./book2.png";
+import book4 from "./book2.png";
+
+import book01 from "./book01.png";
+import book02 from "./book02.png";
+import book03 from "./book03.png";
+import book04 from "./book04.png";
+
+import spider1 from "./spider1.png";
+import spider2 from "./spider2.png";
+import spider3 from "./spider3.png";
+import spider4 from "./spider4.png";
+import spider5 from "./spider5.png";
+import spider6 from "./spider6.png";
+import spider7 from "./spider7.png";
+import spider8 from "./spider8.png";
+
+import spiderman1 from "./spiderman1.png";
+import spiderman2 from "./spiderman2.png";
+import spiderman3 from "./spiderman3.png";
+import spiderman4 from "./spiderman4.png";
+
+export {
+  AvengerSpider,
+  book01,
+  book02,
+  book03,
+  book04,
+  book1,
+  book2,
+  book3,
+  book4,
+  spider1,
+  spider2,
+  spider3,
+  spider4,
+  spider5,
+  spider6,
+  spider7,
+  spider8,
+  spiderman1,
+  spiderman2,
+  spiderman3,
+  spiderman4,
+};
